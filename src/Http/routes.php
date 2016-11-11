9e380d9cfae4739167052291a95ec2a236f0f450
Petar Ristovic <petarristovic77@gmail.com>
2016-11-11 21:30:19 +0100

    Update routes

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,3 +1,13 @@
 <?php
 
+Route::group(['middleware' => 'web', 'namespace' => 'Softwaretours\Site\Http\Controllers'], function()
+{
 
+	Route::get('/{pageName?}',['as' => 'page', 'uses' => 'PageController@page']);
+
+	/*  LOAD MORE POSTS */
+	Route::post('load-posts',['as' => 'posts.load-more', 'uses' => 'PageController@loadPosts']);
+	Route::post('/contactForm', ['uses' => 'PageController@contactForm'])->name('contactForm');
+
+
+});
