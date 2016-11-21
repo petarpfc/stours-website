1a8e62c5faa86d7cb6e2cd2f24a9ef91062814db
Petar Ristovic <petarristovic77@gmail.com>
2016-11-21 15:25:49 +0100

    Update favicon

--- src/Resources/views/layouts/master.blade.php
+++ src/Resources/views/layouts/master.blade.php
@@ -11,7 +11,8 @@
 
 		<meta name="csrf_token" content="{{ csrf_token() }}">
         <link rel="apple-touch-icon" href="{{URL::asset($user['favicon'])}}">
-        <link rel="icon" href="{{URL::asset($user['favicon'])}}">
+        
+        <link rel="icon" href="{{URL::asset($app_public_url.$user['favicon'])}}">
 
 		<!-- Latest compiled and minified CSS -->
 		{{--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">--}}
